9ba4bb8a9ca430f9db582e9011b2c75830f6ccb8
Jacob Blair <jblair@lcog.org>
2019-09-30 09:30:44 -0700

    Add credential submodule.

--- proctools/__init__.py
+++ proctools/__init__.py
@@ -1,5 +1,6 @@
 """Processing pipeline assistance library."""
 from . import communicate  # pylint: disable=unused-import
+from . import credential  # pylint: disable=unused-import
 
 
 __all__ = []
